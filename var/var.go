630aa4c276562568afabf97b0d0fb26488b48c8f
huangankun <646741916@qq.com>
2019-10-14 17:48:15 +0800

    GO语言学习

---
+++ var/var.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "Runoob"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	f := "test"
+	fmt.Println(f)
+
+	var d, h = 123, "hello"
+	k, m := 123, "asdqwe"
+	fmt.Println(d, h, k, m)
+}
