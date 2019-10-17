19573ab78757d09ea6da289edaf4fd944e4fdf41
huangankun <646741916@qq.com>
2019-10-17 09:29:16 +0800

    go学习

--- var/var.go
+++ var/var.go
@@ -1,18 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main() {
-	var a string = "Runoob"
-	fmt.Println(a)
-
-	var b, c int = 1, 2
-	fmt.Println(b, c)
-
-	f := "test"
-	fmt.Println(f)
-
-	var d, h = 123, "hello"
-	k, m := 123, "asdqwe"
-	fmt.Println(d, h, k, m)
+type field struct {
+	name string
 }
+
+func (p *field) print() {
+	fmt.Println(p.name)
+}
+
+// // 错误示例
+// func main() {
+// 	data := []field{{"one"}, {"two"}, {"three"}}
+// 	for _, v := range data {
+// 		go v.print()
+// 	}
+// 	time.Sleep(3 * time.Second)
+// 	// 输出 three three three
+// }
+
+// 正确示例
+func main() {
+	data := []*field{{"one"}, {"two"}, {"three"}}
+	for _, v := range data { // 此时迭代值 v 是三个元素值的地址，每次 v 指向的值不同
+		go v.print()
+	}
+	time.Sleep(3 * time.Second)
+	// 输出 one two three
+}
+
+// // 正确示例
+// func main() {
+// 	data := []field{{"one"}, {"two"}, {"three"}}
+// 	for _, v := range data {
+// 		v := v
+// 		go v.print()
+// 	}
+// 	time.Sleep(3 * time.Second)
+// 	// 输出 one two three
+// }
